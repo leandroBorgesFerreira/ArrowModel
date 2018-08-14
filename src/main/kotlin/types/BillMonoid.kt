f7e7c46c2fb9d8bbef750daccca9740831d39fc1
Leandro Borges Ferreira <lehen01@gmail.com>
2018-08-13 22:54:08 -0300

    fixing Monoids

--- src/main/kotlin/types/BillMonoid.kt
+++ src/main/kotlin/types/BillMonoid.kt
@@ -10,5 +10,9 @@ interface BillMonoid : Monoid<Bill> {
 
     override fun empty(): Bill = Bill(0.0, LocalDate.MIN)
 
-    override fun Bill.combine(b: Bill): Bill = Bill(this.amount + b.amount, this.dueDate)
+    override fun Bill.combine(b: Bill): Bill = Bill(
+            this.amount + b.amount,
+            localDateMonoid().combineAll(this.dueDate, b.dueDate)
+    )
 }
+
